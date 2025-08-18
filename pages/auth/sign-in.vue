609140fb4bb0fdea5618caca6f859eff512d58a5
TienLC <tienlc@innocom.vn>
2025-08-18 18:34:46 +0700

    update import component auth

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,7 +1,5 @@
 <template>
-    <SignInForm />
+    <AuthSignInForm />
 </template>
 <script setup lang="ts">
-import { ref, defineAsyncComponent, nextTick, watch, computed } from 'vue'
-const SignInForm = defineAsyncComponent(() => import('@/components/auth/SignInForm.vue'))
 </script>
--- pages/auth/sign-up.vue
+++ pages/auth/sign-up.vue
@@ -1,7 +1,5 @@
 <template>
-    <SignUpForm />
+    <AuthSignUpForm />
 </template>
 <script setup lang="ts">
-import { ref, defineAsyncComponent, nextTick, watch, computed } from 'vue'
-const SignUpForm = defineAsyncComponent(() => import('@/components/auth/SignUpForm.vue'))
 </script>
